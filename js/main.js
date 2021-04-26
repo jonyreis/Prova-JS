71f810ecfef672365f61d0297dac2e8d54eceb88
Jony Reis <jonyreiscardoso@gmail.com>
2021-04-26 15:28:04 -0400

    feat: add querySlector on button-game

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
-(function () {
+(function (win, doc) {
+  'use strict'
+
   console.log('Main.js')
-})()
+
+  const $buttonGame = doc.querySelectorAll('[data-js="button-game"]')
+
+  function initialize() {
+    initEvents()
+  }
+
+  function initEvents() {
+    Array.prototype.forEach.call($buttonGame, (button) => {
+      button.addEventListener('click', (event) => handleClickGame(event), false)
+    })
+  }
+
+  function getJson() {
+    const ajax = new XMLHttpRequest()
+    ajax.open("GET", "./games.json")
+    ajax.send()
+    ajax.addEventListener("readystatechange", () => getCompanyInfo(ajax), false)
+  }
+
+  function getCompanyInfo(ajax) {
+    if (ajax.readyState === 4 && ajax.status === 200) {
+      const data = JSON.parse(ajax.responseText)
+      console.log(data)
+    }
+  }
+
+  function handleClickGame(event) {
+    console.log(event.target.textContent)
+    getJson()
+  }
+
+  initialize()
+
+})(window, document)
