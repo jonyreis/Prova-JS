e96c1005304e294e69da683739a11e9b8178807e
joseturing <boilerplatex@gmail.com>
2021-04-27 11:15:50 -0400

    feat: add getGameInfo function

--- js/main.js
+++ js/main.js
@@ -4,34 +4,34 @@
   console.log('Main.js')
 
   const $buttonGame = doc.querySelectorAll('[data-js="button-game"]')
+  const $descriptionGame = doc.querySelector('[data-js="description-game"]')
+
+  let dataGame = []
 
   function initialize() {
     initEvents()
+    getJson()
   }
 
   function initEvents() {
     Array.prototype.forEach.call($buttonGame, (button) => {
-      button.addEventListener('click', (event) => handleClickGame(event), false)
+      button.addEventListener('click', (event) => getJson(event.target.textContent), false)
     })
   }
 
-  function getJson() {
+  function getJson(nameGame) {
     const ajax = new XMLHttpRequest()
     ajax.open("GET", "./games.json")
     ajax.send()
-    ajax.addEventListener("readystatechange", () => getCompanyInfo(ajax), false)
+    ajax.addEventListener("readystatechange", () => getGameInfo(ajax, nameGame), false)
   }
 
-  function getCompanyInfo(ajax) {
+  function getGameInfo(ajax, nameGame) {
     if (ajax.readyState === 4 && ajax.status === 200) {
-      const data = JSON.parse(ajax.responseText)
-      console.log(data)
+      const { types } = JSON.parse(ajax.responseText)
+      dataGame = types.filter(item => item.type === nameGame)
+      $descriptionGame.innerHTML = dataGame.length > 0 ? dataGame[0].description : types[1].description
     }
-  }
-
-  function handleClickGame(event) {
-    console.log(event.target.textContent)
-    getJson()
   }
 
   initialize()
