dfb2a13c2afa50cb003171bf5354782fe47d0797
joseturing <boilerplatex@gmail.com>
2021-04-27 15:31:20 -0400

    feat: add completeGame function

--- js/main.js
+++ js/main.js
@@ -2,23 +2,13 @@
   'use strict'
 
   const $buttonGame = doc.querySelectorAll('[data-js="button-game"]')
+  const $buttonCompleteGame = doc.querySelector('[data-js="complete-game"]')
   const $numbersContainer = doc.querySelector('[data-js="numbers"]')
   const $descriptionGame = doc.querySelector('[data-js="description-game"]')
 
   let dataGame = []
 
-  function initialize() {
-    initEvents()
-    getJson()
-  }
-
-  function initEvents() {
-    Array.prototype.forEach.call($buttonGame, (button) => {
-      button.addEventListener('click', (event) => getJson(event.target.value), false)
-    })
-  }
-
-  function getJson(nameGame) {
+  function getJson(nameGame = "Mega-Sena") {
     const ajax = new XMLHttpRequest()
     ajax.open("GET", "./games.json")
     ajax.send()
@@ -28,9 +18,11 @@
   function getGameInfo(ajax, nameGame) {
     if (ajax.readyState === 4 && ajax.status === 200) {
       const { types } = JSON.parse(ajax.responseText)
+
       dataGame = types.filter(item => item.type === nameGame)
-      $descriptionGame.innerHTML = dataGame.length > 0 ? dataGame[0].description : types[1].description
-      dataGame.length > 0 ? createNumbers(dataGame[0].range) : createNumbers(types[1].range)
+      $descriptionGame.innerHTML = dataGame[0].description
+
+      createNumbers(dataGame[0].range)
     }
   }
 
@@ -41,6 +33,40 @@
     }
   }
 
-  initialize()
+  function selectNumber(arr, range) {
+    console.log(arr, range)
+    $numbersContainer.innerHTML = ''
+    for (let i = 1; i <= range; i++) {
+      if (arr.indexOf(i) === -1) {
+        $numbersContainer.innerHTML += `<div class="number">${i}</div>`
+      } else {
+        $numbersContainer.innerHTML += `<div class="number selected">${i}</div>`
+      }
+    }
+  }
+
+  function completeGame() {
+    let arr = []
+
+    while (arr.length < dataGame[0]['max-number']) {
+        var numberAleatory = Math.ceil(Math.random() * dataGame[0].range)
+
+      if (arr.indexOf(numberAleatory) == -1) {
+        arr.push(numberAleatory);
+      }
+    }
+
+    selectNumber(arr, dataGame[0].range)
+  }
+
+
+  Array.prototype.forEach.call($buttonGame, (button) => {
+    button.addEventListener('click', (event) => getJson(event.target.value), false)
+  })
+
+  $buttonCompleteGame.addEventListener('click', completeGame, false)
+
+
+  getJson()
 
 })(window, document)
