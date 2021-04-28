1f826d314d6fcf7126cde88b216b3fb6ce7d3cee
Jony <jonyreiscardoso@gmail.com>
2021-04-28 11:12:11 -0400

    feat: add addToCart  function

--- js/main.js
+++ js/main.js
@@ -4,10 +4,15 @@
   const $buttonGame = doc.querySelectorAll('[data-js="button-game"]')
   const $buttonCompleteGame = doc.querySelector('[data-js="complete-game"]')
   const $buttonClearGame = doc.querySelector('[data-js="clear-game"]')
+  const $buttonAddToCart = doc.querySelector('[data-js="add-to-cart"]')
+
   const $numbersContainer = doc.querySelector('[data-js="numbers"]')
   const $descriptionGame = doc.querySelector('[data-js="description-game"]')
+  const $addBetToCart = doc.querySelector('[data-js="bets"]')
 
   let dataGame = []
+  let arrayRandomNumber
+  let bets = []
   let arr
 
   function getJson(nameGame = "Mega-Sena") {
@@ -35,11 +40,11 @@
     }
   }
 
-  function selectNumber(arr, range) {
-    console.log(arr, range)
+  function selectNumber(arrayRandomNumber, range) {
+    console.log(arrayRandomNumber, range)
     $numbersContainer.innerHTML = ''
     for (let i = 1; i <= range; i++) {
-      if (arr.indexOf(i) === -1) {
+      if (arrayRandomNumber.indexOf(i) === -1) {
         $numbersContainer.innerHTML += `<div class="number">${i}</div>`
       } else {
         $numbersContainer.innerHTML += `<div class="number selected">${i}</div>`
@@ -49,22 +54,64 @@
 
   function completeGame() {
     let randomNumber = ''
-    arr = []
+    arrayRandomNumber = []
 
-    while (arr.length < dataGame[0]['max-number']) {
+    while (arrayRandomNumber.length < dataGame[0]['max-number']) {
       randomNumber = Math.ceil(Math.random() * dataGame[0].range)
 
-      if (arr.indexOf(randomNumber) == -1) {
-        arr.push(randomNumber)
+      if (arrayRandomNumber.indexOf(randomNumber) == -1) {
+        arrayRandomNumber.push(randomNumber)
       }
     }
 
-    selectNumber(arr, dataGame[0].range)
+    selectNumber(arrayRandomNumber, dataGame[0].range)
   }
 
   function clearGame() {
     createNumbers(dataGame[0].range)
+    arrayRandomNumber = []
+  }
+
+  function createBet(arrayNumbers) {
+    arrayNumbers.sort(function(a, b) {
+      return a - b
+    })
+
+    bets.push({
+      arrayNumbers: arrayNumbers,
+      type: dataGame[0].type,
+      price: dataGame[0].price,
+      color: dataGame[0].color
+    })
+
+  }
+
+  function convertToCurrency(number) {
+    return number.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+  }
+
+
+  function addToCart() {
+    if (arrayRandomNumber.length > 1) {
+      createBet(arrayRandomNumber)
+      $addBetToCart.innerHTML = ''
+      bets.map(bet => {
+        $addBetToCart.innerHTML += `
+          <div class="bet">
+            <img src="/assets/trash.svg" alt="trash" />
+            <div class="separator" style="background-color: ${bet.color};"></div>
+            <div class="bet-info">
+              <h4>${bet.arrayNumbers}</h4>
+              <div>
+                <strong style="color: ${bet.color};">${bet.type}</strong><span>${convertToCurrency(bet.price)}</span>
+              </div>
+            </div>
+          </div>
+        `
+      })
+
     arr = []
+    }
   }
 
   Array.prototype.forEach.call($buttonGame, (button) => {
@@ -73,7 +120,7 @@
 
   $buttonCompleteGame.addEventListener('click', completeGame, false)
   $buttonClearGame.addEventListener('click', clearGame, false)
-
+  $buttonAddToCart.addEventListener('click', addToCart, false)
 
   getJson()
 
